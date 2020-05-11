2b6b237400e56a398d2f5a9e5b0d494c45ec6a9a
rythemneeru <rythemneeru>
2020-05-11 14:02:44 +0530

    commit

--- Ass/src/app/app.component.ts
+++ Ass/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ass';
+  title = 'Angulater Master Class - Lecture 1 Assignment : Simple Calculator';  
+  num1: any;
+  num2: any;
+  result: any;
+  opval: string = "opval";
+
+  calculate(x) {
+    let firstval = parseInt(this.num1);
+    let secondval = parseInt(this.num2);
+    let result = 0;
+    switch (x) {
+      case 'add':
+        result = firstval + secondval;
+        this.opval = "+";
+        break;
+      case 'minus':
+        result = firstval - secondval;
+        this.opval = "-";
+        break;
+      case 'multiplication':
+        result = firstval * secondval;
+        this.opval = "x";
+        break;
+        case 'divide':
+          result = firstval / secondval;
+        this.opval = "/";
+        break;      
+        case 'clear':
+          this.num1 = "";
+          this.num2 = "";
+          this.result = "";
+          this.opval = "opval";
+        break;
+    }
+    this.result = result;
+  }
+  
 }
